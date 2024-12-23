15859dbc9b3c60a955bf651d4ebc50ed2aec25ed
Anang-Programmer <jakaperdana2309@gmail.com>
2024-12-23 20:09:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
